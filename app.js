d2f2f98a47a89f2196a3347b7085e82c5c46e859
s-moe <sarahannmoe@gmail.com>
2021-10-22 08:48:19 -0700

    image, value attribute, random number

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ const app = Vue.createApp({
     return {
       name: "Sarah",
       age: 37,
+      image:
+        "https://hips.hearstapps.com/countryliving.cdnds.net/17/47/1511194376-cavachon-puppy-christmas.jpg",
     };
   },
   methods: {
     ageInFiveYears() {
       let newAge = this.age + 5;
       return newAge;
+    },
+    favoriteNumber() {
+      let randomNumber = Math.random();
+      return randomNumber;
     },
   },
 });
